2989c7bcc77722c706bb2dad9474214286ec4a48
elenarumianceva <arefuthescroob@gmail.com>
2022-06-11 14:40:34 +0300

    added versioning with gulp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AppActionsService } from './store/app-actions.service';
 import * as fromFeature from './store';
-import { Beer } from './interfaces/punk-beer-interface';
+import { environment } from '../environments/environment';
+import { nullSafeIsEquivalent } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,8 @@ import { Beer } from './interfaces/punk-beer-interface';
 })
 export class AppComponent implements OnInit {
   beers$: Observable<any>;
+
+  version!: string; // Our version
 
   constructor(
     private readonly storeService: AppActionsService,
@@ -21,5 +24,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.storeService.getBeers();
+
+    this.version = environment.version; // Consume the version number from environment
   }
 }
